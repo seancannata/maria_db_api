811d83e5e0d4270027d439a2efaccbf31bc2f194
Sean Cannata <scannata@redhat.com>
2020-08-04 14:32:14 -0400

    change from hardcoded to environment variables for config.

--- app.py
+++ app.py
@@ -2,17 +2,34 @@
 import flask
 import json
 import mariadb
+import os
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 # configuration used to connect to MariaDB
+'''
 config = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
     'password': 'Password123!',
     'database': 'demo'
+}
+
+export HOST=127.0.0.1
+export PORT=3306
+export USER=root
+export PASSWORD=Password123!
+export DATABASE=demo
+'''
+
+config = {
+    'host': str(os.environ['HOST']),
+    'port': int(os.environ['PORT']),
+    'user': str(os.environ['USER']),
+    'password': str(os.environ['PASSWORD']),
+    'database': str(os.environ['DATABASE'])
 }
 
 # route to return all people
